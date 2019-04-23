de621c6780c911fbde7c74b12fa77f5074e1a0df
SuperAmi3401 <primemhd@outlook.com>
2019-04-23 22:34:49 +0800

    DiskDriver

--- include/DiskDriver.h
+++ include/DiskDriver.h
@@ -12,12 +12,18 @@
 class DiskDriver{
   private:
     bool isMounted=false;
+    FileFd DiskFd;  //挂载磁盘文件的句柄
+    DiskBlock* DiskMemAddr;
+    const char *TAG;
   public:
-    void mount();
-    void unmount();
-    DiskBlock* getBlk();
-    void readBlk();
-    void writeBlk();
+    DiskDriver();
+    ~DiskDriver();
+    void mount();//安装img磁盘
+    void unmount();//卸载磁盘
+    DiskBlock* getBlk(int blockNum);//获得指向块的指针
+    void readBlk(int blockNum);//读取块
+    void writeBlk(int blockNum,const DiskBlock& blk);//写入块
+    bool isDiskMounted();
 
 
 };
--- include/define.h
+++ include/define.h
@@ -5,11 +5,18 @@
 #include <iostream>  //大家都懂
 #include <cstring>   //用到str函数
 #include <string> //沈坚不让用的String类
-#include <stdint.h>
+#include <stdint.h> //需要用到比特类型
+#include <fcntl.h> //open系统调用
+#include <unistd.h> //lseek函数
+#include <sys/mman.h>  //mmap函数
+#include <File.h>
 
 //const和define的区别》
 #define DISK_BLOCK_SIZE 4096
+#define DISK_SIZE (128*1024*1024)
 #define DISK_IMG_FILEPATH "../disk.img"
+
+typedef int FileFd; //文件句柄，实际上就是一个int
 enum INSTRUCT {
     ERROR_INST = -1,
     MOUNT,
